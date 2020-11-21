a5ca78443ca8381c7fb8e24ffe3e748327700c09
Vasile Home <chisinau2000>
2020-11-21 10:42:15 +0200

    appbar 1

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import logo from '../../assets/logo.svg';
 
 //https://material-ui.com/components/app-bar/
 export default function Header(props) {
+
+    const useStyles = makeStyles(theme => ({
+        toolbarMargin: {
+            ...theme.mixins.toolbar,
+            marginBottom:'2em'
+        },
+        logo: {
+            height: '6em'
+        }
+    }));
+
+    const classes = useStyles();
+
     return (
-        <AppBar position='fixed' color='primary'>
-            <Toolbar>
-                <Typography variant='h6'>
-                    Ceva development
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position='fixed' color='primary'>
+                <Toolbar disableGutters>
+                    {/*<Typography variant='h5' color='secondary'> Ceva development </Typography>*/}
+                    <img src={logo} className={classes.logo} alt="logo"/>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.toolbarMargin}/>
+        </>
     );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,6 +1,7 @@
 //npm i @material-ui/styles
 //https://material-ui.com/customization/theming/#createmuitheme-options-args-theme
 //https://material-ui.com/customization/default-theme/
+
 import {createMuiTheme} from '@material-ui/core/styles';
 
 const arcBlue = '#0B72B9';
@@ -19,6 +20,6 @@ export default createMuiTheme({
         },
     },
     typography:{
-
+        h3:{}
     }
 });
